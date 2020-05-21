d08b735e22dfd754f03e46c47a99ce0e63b82f74
vhavhugo <vhavhugo@hotmail.com>
2020-05-21 11:39:17 -0300

    estilizando o jogo

--- public/js/main.js
+++ public/js/main.js
@@ -39,6 +39,7 @@ function inicializaCronometro(){
             if(tempoRestantante < 1){
                 campo.attr("disabled", true);
                 clearInterval(cronometroID);
+                campo.addClass("campo-desativado");
             }
         },1000);
     });
