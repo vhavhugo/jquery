42fa25e2950e771c47b67eed330d8cf2c20fb4f1
vhavhugo <vhavhugo@hotmail.com>
2020-05-21 11:20:32 -0300

    Reiniciar o jogo

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,55 @@
-var frase = $(".frase").text();
-var numPalavaras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavaras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-campo.on("input", function(){
-    var conteudo = campo.val();
-    
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
 
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
-    
+// $(document).ready(function(){}) == $(function(){})
+$(function(){
+    atualizaTamnhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
-var tempoRestantante = $("#tempo-digitacao").text();
-campo.one("focus", function(){
-    var cronometroID = setInterval(function(){
-        tempoRestantante--;
-        $("#tempo-digitacao").text(tempoRestantante);
-        if(tempoRestantante < 1){
-            campo.attr("disabled", true);
-            clearInterval(cronometroID);
-        }
-    },1000);
-});
+
+function atualizaTamnhoFrase(){
+    var frase = $(".frase").text();
+    var numPalavaras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavaras);
+}
+
+    function inicializaContadores(){
+        campo.on("input", function(){
+            var conteudo = campo.val();
+            
+            var qtdPalavras = conteudo.split(/\S+/).length - 1;
+            $("#contador-palavras").text(qtdPalavras);
+        
+            var qtdCaracteres = conteudo.length;
+            $("#contador-caracteres").text(qtdCaracteres);
+            
+        });
+    }
+
+
+function inicializaCronometro(){
+    var tempoRestantante = $("#tempo-digitacao").text();
+    campo.one("focus", function(){
+        var cronometroID = setInterval(function(){
+            tempoRestantante--;
+            $("#tempo-digitacao").text(tempoRestantante);
+            if(tempoRestantante < 1){
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+            }
+        },1000);
+    });
+}
+
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();    
+}
