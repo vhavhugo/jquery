435894f6c181c5136c6516f5aebc99ca0eee72c2
vhavhugo <vhavhugo@hotmail.com>
2020-05-31 22:36:21 -0300

    biblioteca tooltipster

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Curso Jquery Alura - Alura Typer, jogo de marcar o tempo que vc escreve o texto e várias funcionalidades.
 
 
+https://selectize.github.io/selectize.js/
--- public/js/placar.js
+++ public/js/placar.js
@@ -3,7 +3,7 @@ $("#botao-sync").click(sincronizaPlacar);
 
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Hugo"
+    var usuario = $("#usuarios").val();
     var numPalavras = $("#contador-palavras").text();
 
     var linha = novaLinha(usuario, numPalavras);
@@ -77,8 +77,14 @@ function sincronizaPlacar(){
     };
 
     //salvar o placar
-    $.post("http://localhost:3000/placar", dados, function(){
-        console.log("Salvou o placar no servidor");
+    $.post("http://localhost:3000/placar",dados,function(){
+        $(".tooltip").tooltipster("open").tooltipster("content","Sucesso as sincronizar");
+    }).fail(function(){
+        $(".tooltip").tooltipster("open").tooltipster("content", "Falha ao sincronizar");
+    }).always(function(){
+        setTimeout(function(){
+            $(".tooltip").tooltipster("close");
+        },1200);
     });
 }
 
